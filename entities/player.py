8561c04ba3afc57b96f3825d0e01f6afe31eb30b
notnine <g.jeremy@mail.utoronto.ca>
2022-01-28 14:02:34 -0500

    Update Player.py
    added the following attributes to player:
    name
    level
    friendlist
    
    and their corresponding setters and getters

--- entities/player.py
+++ entities/player.py
@@ -1,1 +1,38 @@
-# test
+class Player:
+    """An object of Player"""
+    def __init__(self, name, level, friend_list):
+        """
+        Initialize a Player with the following attributes:
+        name: String
+        level: int
+        friend_list: List[Player]
+        """
+        self.name = name
+        self.level = level
+        self.friend_list = friend_list
+
+    def set_name(self, name):
+        """Set name of player"""
+        self.name = name
+
+    def set_level(self, level):
+        """Set level of player"""
+        self.level = level
+
+    def set_friend_list(self, friend_list):
+        """"""
+        self.friend_list = friend_list
+
+    def get_name(self) -> String:
+        """Return name of player"""
+        return self.name
+
+    def get_level(self) -> int:
+        """Return level of player"""
+        return self.level
+
+    def get_friend_list(self) -> List[Player]:
+        """Return Player's friend list"""
+        return self.friend_list
+
+# ToDo: Privatize appropriate attributes
